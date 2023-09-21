e823b5aa252a4251c621b048928b7a2d89357bae
gezrylclariz <gezrylclarizg@gmail.com>
2023-09-21 23:04:05 +0800

    migration: ipcr forms

---
+++ app/Http/Controllers/IpcrFormController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreIpcrFormRequest;
+use App\Http\Requests\UpdateIpcrFormRequest;
+use App\Models\IpcrForm;
+
+class IpcrFormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreIpcrFormRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(IpcrForm $ipcrForm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(IpcrForm $ipcrForm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateIpcrFormRequest $request, IpcrForm $ipcrForm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(IpcrForm $ipcrForm)
+    {
+        //
+    }
+}
