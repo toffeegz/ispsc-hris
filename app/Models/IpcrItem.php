6b97b218391e95da19572f1a2ef008cf40356ee1
gezrylclariz <gezrylclarizg@gmail.com>
2023-09-21 23:04:14 +0800

    migration: ipcr items

---
+++ app/Models/IpcrItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IpcrItem extends Model
+{
+    use HasFactory;
+}
