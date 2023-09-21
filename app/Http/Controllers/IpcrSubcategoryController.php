372255a346b6cf4bdf1cd04466e0441d57b602c0
gezrylclariz <gezrylclarizg@gmail.com>
2023-09-21 22:23:54 +0800

    generate model, migration, seeder, factory, and a CRUD controller.

---
+++ app/Http/Controllers/IpcrSubcategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreIpcrSubcategoryRequest;
+use App\Http\Requests\UpdateIpcrSubcategoryRequest;
+use App\Models\IpcrSubcategory;
+
+class IpcrSubcategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreIpcrSubcategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(IpcrSubcategory $ipcrSubcategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(IpcrSubcategory $ipcrSubcategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateIpcrSubcategoryRequest $request, IpcrSubcategory $ipcrSubcategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(IpcrSubcategory $ipcrSubcategory)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TrainingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTrainingRequest;
+use App\Http\Requests\UpdateTrainingRequest;
+use App\Models\Training;
+
+class TrainingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTrainingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Training $training)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Training $training)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTrainingRequest $request, Training $training)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Training $training)
+    {
+        //
+    }
+}
