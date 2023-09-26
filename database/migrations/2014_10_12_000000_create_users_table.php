4529c3e6de34e0f49c6814a00755c65a91b09f75
gezrylclariz <gezrylclarizg@gmail.com>
2023-09-26 22:19:15 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('google_id')->unique()->nullable();
-            $table->uuid('role_id');
+            $table->uuid('role_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
