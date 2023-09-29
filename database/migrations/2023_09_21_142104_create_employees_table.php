71b69e71ba73e0a49cb3eaf2411e6a665d3452c4
gezrylclariz <gezrylclarizg@gmail.com>
2023-09-29 10:47:07 +0800

    fix: employees table adjustment

--- database/migrations/2023_09_21_142104_create_employees_table.php
+++ database/migrations/2023_09_21_142104_create_employees_table.php
@@ -14,20 +14,22 @@ return new class extends Migration
         Schema::create('employees', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('employee_id')->nullable();
-            $table->string('surname');
+            $table->string('last_name');
             $table->string('first_name');
             $table->string('middle_name')->nullable();
             $table->string('name_ext')->nullable();
             $table->date('birth_date')->nullable();
             $table->string('birth_place')->nullable();
-            $table->unsignedTinyInteger('sex')->nullable();
-            $table->unsignedTinyInteger('civil_status')->nullable();
+            $table->char('sex', 50)->nullable();
+            $table->char('civil_status', 100)->nullable();
             $table->char('citizenship', 100)->nullable();
             $table->string('email')->unique()->nullable();
             $table->string('tel_no')->nullable();
             $table->string('mobile_no')->nullable();
             $table->date('date_hired')->nullable();
             $table->uuid('user_id')->nullable();
+            $table->uuid('department_id')->nullable();
+            $table->uuid('position_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
