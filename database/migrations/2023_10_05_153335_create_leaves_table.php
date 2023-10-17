2578e8c3f3aad12ee215930c28d7d55b6d02db5f
gezrylclariz <gezrylclarizg@gmail.com>
2023-10-17 20:10:06 +0800

    fix: id into primary

--- database/migrations/2023_10_05_153335_create_leaves_table.php
+++ database/migrations/2023_10_05_153335_create_leaves_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('leaves', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->uuid('employee_id');
             $table->date('date_filed');
             $table->date('date_start');
--- database/migrations/2023_10_05_153347_create_leave_types_table.php
+++ database/migrations/2023_10_05_153347_create_leave_types_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('leave_types', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->text('description')->nullable();
             $table->boolean('is_deletable')->default(true);
