26ae0fd4568cfc0e2a6bb3b061b3b1d0098e6152
gezrylclariz <gezrylclarizg@gmail.com>
2023-10-26 06:06:07 +0800

    feat: evaluation initial

--- app/Models/IpcrEvaluationItem.php
+++ app/Models/IpcrEvaluationItem.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class IpcrEvaluationItem extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid, SoftDeletes;
+
+    protected $fillable = [
+        'evaluation_id',
+        'category_id',
+        'item_id',
+        'major_final_output',
+        'performance_indicators',
+        'target_of_accomplishment',
+        'actual_accomplishments',
+        'rating_q',
+        'rating_e',
+        'rating_t',
+        'rating_a',
+        'remarks',
+    ];
 }
