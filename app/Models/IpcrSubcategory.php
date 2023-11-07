fc04414b1ae4c5b38bd9fc49c44edaa015113385
gezrylclariz <gezrylclarizg@gmail.com>
2023-11-07 09:16:31 +0800

    ipcr subcategory module

--- app/Models/IpcrSubcategory.php
+++ app/Models/IpcrSubcategory.php
@@ -18,4 +18,18 @@ class IpcrSubcategory extends Model
         'parent_id',
     ];
 
+    public function scopeFilter($query, array $filters)
+    {
+        $search = $filters['search'] ?? false;
+        $query
+            ->when($filters['search'] ?? false, 
+            function($query) use($search) {
+                $query->where(function($query) use($search) {
+                    $query->where('name', 'like', '%' . $search . '%')
+                        ->orWhere('weight', 'like', '%' . $search . '%');
+                });
+            }
+        );
+    }
+
 }
