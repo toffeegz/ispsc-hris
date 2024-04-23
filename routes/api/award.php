4eadb3678c3e80e0e946be526ea85e07a98c9ce8
Gezryl <gezrylclarizg@gmail.com>
2024-04-23 18:06:56 +0800

    fix: awards overview

--- routes/api/award.php
+++ routes/api/award.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AwardController;
 
 Route::prefix('awards')->group(function() {
-    Route::get('/overview', [AwardController::class, 'overview'])->name('award.overview');
+    Route::get('/', [AwardController::class, 'overview'])->name('award.overview');
     Route::get('/details', [AwardController::class, 'details'])->name('award.details');
     Route::get('archive', [AwardController::class, 'archive'])->name('award.archive');
     Route::get('/{award}', [AwardController::class, 'show'])->name('award.show');
